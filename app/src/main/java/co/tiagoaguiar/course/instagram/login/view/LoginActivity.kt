6bfc045d751e01666af9ef223fd75345c5662a58
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2023-01-08 20:42:51 -0300

    UI da tela de login criada

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package co.tiagoaguiar.course.instagram.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.tiagoaguiar.course.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+  }
+}
